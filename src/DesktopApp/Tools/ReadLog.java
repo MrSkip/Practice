2ea9399ffb6f251524d93aabcf8524d2a888e89a
MrSkip <dimon.mula@gmail.com>
2015-12-15 19:44:53 +0300

    add support log File

---
+++ src/DesktopApp/Tools/ReadLog.java
@@ -0,0 +1,7 @@
+package DesktopApp.Tools;
+
+/**
+ * Created by Mr Skip on 13.12.2015.
+ */
+public class ReadLog {
+}
---
+++ src/DesktopApp/Tools/Vocabulary/Words.java
@@ -0,0 +1,7 @@
+package DesktopApp.Tools.Vocabulary;
+
+/**
+ * Created by Mr Skip on 13.12.2015.
+ */
+public class Words {
+}
